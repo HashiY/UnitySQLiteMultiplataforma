880973bd9d280afe4452990bf325101d728b2139
VSF <47865897+HashiY@users.noreply.github.com>
2022-09-22 04:04:12 -0300

    First commit
    + Created DatabaseBuilder
    + Created Database Using Code

---
+++ UnitySQLiteMultiplataforma/Assets/Scripts/DatabaseBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class DatabaseBuilder : MonoBehaviour
+{
+    public string DatabaseName; // nome do arquivo
+    protected string databasePath;//caminho para o bd              
+    protected SqliteConnection Connection => new SqliteConnection($"Data source = {this.databasePath};");
+
+    private void Awake()
+    {
+        if (string.IsNullOrEmpty(this.DatabaseName))//se esse nome for null ou vazio
+        {
+            Debug.LogError("Database name is empty!!!!"); // sabe q falta informar o nome do bd
+            return;
+        }
+        CreateDatabaseFileIfNotExists();
+    }
+
+    private void CreateDatabaseFileIfNotExists()//criar o bd se ele nao exitir ainda
+    {
+        //combinar diretorio , caminho ao nosso arquivo de bd
+        this.databasePath = Path.Combine(Application.persistentDataPath, this.DatabaseName);
+
+        if (!File.Exists(this.databasePath))//se nao existe nesse diretotio criar
+        {
+            SqliteConnection.CreateFile(this.databasePath);//criar somente o arquivo
+            Debug.Log($"Database Path: {this.databasePath}");
+        }
+    }
+}
